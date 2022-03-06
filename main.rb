f15fc23f7a66f809085c32a9884618e0c0804f72
Matthiasclee <matthias@matthiasclee.com>
2022-03-06 07:42:13 -0800

    Added loading a wallet

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ puts "2) Load wallet"
 option = $stdin.gets.chomp.to_i
 
 if option == 1
+
   wallet = Wallet.new
 
   print "Do you want to save this wallet to a file? [Y/n] "
@@ -17,6 +18,11 @@ if option == 1
   end
 
 elsif option == 2
+  
+  print "Wallet file: "
+  file = $stdin.gets.chomp
+  
+  wallet = Wallet.new(from: :seedfile, seedfile: file)
 
 else
   puts "Invalid option"
