0d2599d2de805c105e8353efbd4940beba491640
Matthiasclee <matthias@matthiasclee.com>
2022-03-06 08:02:46 -0800

    Misc interface stuff

--- main.rb
+++ main.rb
@@ -1,5 +1,43 @@
 require './wallet.rb'
 
+def cli_interface(wallet)
+  options = {
+    1 => "Wallet address",
+    2 => "Private key",
+    3 => "Generate child wallet"
+  }
+
+  loop do
+    options.keys.each do |opt|
+      puts "#{opt}) #{options[opt]}"
+    end
+
+    option = $stdin.gets.chomp.to_i
+
+    if option == 1
+      puts "\n#{wallet.address}"
+    end
+    
+    if option == 2
+      puts "\n#{wallet.private_key}"
+    end
+
+    if option == 3
+      print "Wallet number: "
+      number = $stdin.gets.chomp.to_i
+
+      print "Wallet depth: "
+      depth = $stdin.gets.chomp.to_i
+
+      child = wallet.generate_child number, depth
+
+      puts
+      puts child.address
+      puts child.private_key
+    end
+  end
+end
+
 puts "1) New wallet"
 puts "2) Load wallet"
 
@@ -17,12 +55,16 @@ if option == 1
     File.write( $stdin.gets.chomp, wallet.seed )
   end
 
+  cli_interface wallet
+
 elsif option == 2
   
   print "Wallet file: "
   file = $stdin.gets.chomp
   
   wallet = Wallet.new(from: :seedfile, seedfile: file)
+
+  cli_interface wallet
 
 else
   puts "Invalid option"
