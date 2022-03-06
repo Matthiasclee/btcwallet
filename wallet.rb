b570ac2e6770bef194accb11acfe92a27fb067d2
Matthiasclee <matthias@matthiasclee.com>
2022-03-06 07:21:22 -0800

    Added children

--- wallet.rb
+++ wallet.rb
@@ -18,6 +18,20 @@ class Wallet
     end
   end
 
+  def generate_children(number, depth=1)
+    children = []
+    number.times do |num|
+      node = @master.node_for_path "m/#{num}/#{depth}"
+      children << node
+    end
+
+    return children
+  end
+
+  def generate_child(number=1, depth=1)
+    return self.generate_children(number, depth)[0]
+  end
+
   def private_key
     @master_private_key.to_s
   end
