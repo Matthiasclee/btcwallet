3d06485b2b2334e57a3ae14d947ab01d5a2e1297
Matthiasclee <matthias@matthiasclee.com>
2022-03-06 07:39:21 -0800

    Added main.rb

---
+++ main.rb
@@ -0,0 +1,24 @@
+require './wallet.rb'
+
+puts "1) New wallet"
+puts "2) Load wallet"
+
+option = $stdin.gets.chomp.to_i
+
+if option == 1
+  wallet = Wallet.new
+
+  print "Do you want to save this wallet to a file? [Y/n] "
+  save = $stdin.gets.chomp
+
+  if save != "n"
+    print "Filename: "
+    File.write( $stdin.gets.chomp, wallet.seed )
+  end
+
+elsif option == 2
+
+else
+  puts "Invalid option"
+  exit
+end
